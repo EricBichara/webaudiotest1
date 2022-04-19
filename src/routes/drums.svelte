35d51c96f844b033916d103adb1ace5dd48a0abd
Eric <eric.bichara@gmail.com>
2022-04-19 21:24:02 +0200

    drum machine

--- src/routes/drums.svelte
+++ src/routes/drums.svelte
@@ -1,13 +1,18 @@
 <script>
     import {onMount} from "svelte";
 
-
     let frequency = 100;
     let mixGainValue = 1;
     let bpm = 100;
+    let nextNoteTime = 0.0;
+    let current16thNote = 0;
+    const scheduleAheadTime = 0.1;
+    let notesInQueue = [];
+    let timerID;
+    const lookahead = 25.0;
 
-    let grid = [[1, false, false, false], [1, false, false, false], [1, false, false, false], [1, false, false, false]];
-
+    let grid = [[true, false, false, false], [true, false, false, false], [true, false, false, false], [true, false, false, false], [true, false, false, false], [true, false, false, false], [true, false, false, false], [true, false, false, false]];
+    let gridLabel = ['Kick', 'Snare', 'HighHat', 'Clap'];
 
     let osc = null;
     let osc2;
@@ -30,44 +35,97 @@
         mixGain.connect(audioContext.destination);
     })
 
-    function playKick() {
-        if (osc != null) {
-            osc.stop(audioContext.currentTime);
-            osc2.stop(audioContext.currentTime);
-        }
+    function nextNote() {
+        // Advance current note and time by a 16th note...
+        const secondsPerBeat = 60.0 / bpm;    // Notice this picks up the CURRENT
+                                              // tempo value to calculate beat length.
+        nextNoteTime += secondsPerBeat;    // Add beat length to last beat time
 
+        current16thNote++;    // Advance the beat number, wrap to zero
+        if (current16thNote === 8) {
+            current16thNote = 0;
+        }
+    }
+
+    function scheduleNote(beatNumber, time) {
+        // push the note on the queue, even if we're not playing.
+        notesInQueue.push({note: beatNumber, time: time});
+
+        const column = grid[beatNumber];
+
+        if (column[0]) {
+            console.log('schedule play kick', time, beatNumber);
+            playKick(time);
+        }
+        if (column[1]) {
+            console.log('schedule play snare', time, beatNumber);
+            playSnare(time);
+        }
+    }
+
+    function scheduler() {
+        // while there are notes that will need to play before the next interval,
+        // schedule them and advance the pointer.
+        while (nextNoteTime < audioContext.currentTime + scheduleAheadTime) {
+            scheduleNote(current16thNote, nextNoteTime);
+            nextNote();
+        }
+        timerID = window.setTimeout(scheduler, lookahead);
+    }
+
+    function playKick(time) {
         osc = audioContext.createOscillator();
         osc2 = audioContext.createOscillator();
 
         osc.type = "triangle";
         osc2.type = "sine";
 
-        gainOsc.gain.setValueAtTime(1, audioContext.currentTime);
-        gainOsc.gain.exponentialRampToValueAtTime(0.001, audioContext.currentTime + 0.5);
+        gainOsc.gain.setValueAtTime(1, time);
+        gainOsc.gain.exponentialRampToValueAtTime(0.001, time + 0.5);
 
-        gainOsc2.gain.setValueAtTime(1, audioContext.currentTime);
-        gainOsc2.gain.exponentialRampToValueAtTime(0.001, audioContext.currentTime + 0.5);
+        gainOsc2.gain.setValueAtTime(1, time);
+        gainOsc2.gain.exponentialRampToValueAtTime(0.001, time + 0.5);
 
-        osc.frequency.setValueAtTime(frequency, audioContext.currentTime);
-        osc.frequency.exponentialRampToValueAtTime(0.001, audioContext.currentTime + 0.5);
+        osc.frequency.setValueAtTime(frequency, time);
+        osc.frequency.exponentialRampToValueAtTime(0.001, time + 0.5);
 
-        osc2.frequency.setValueAtTime(frequency - 70, audioContext.currentTime);
-        osc2.frequency.exponentialRampToValueAtTime(0.001, audioContext.currentTime + 0.5);
+        osc2.frequency.setValueAtTime(frequency - 70, time);
+        osc2.frequency.exponentialRampToValueAtTime(0.001, time + 0.5);
 
         mixGain.gain.value = mixGainValue;
 
         osc.connect(gainOsc);
         osc2.connect(gainOsc2);
 
-        osc.start(audioContext.currentTime);
-        osc2.start(audioContext.currentTime);
+        osc.start(time);
+        osc2.start(time);
 
-        osc.stop(audioContext.currentTime + 0.5);
-        osc2.stop(audioContext.currentTime + 0.5);
+        osc.stop(time + 0.5);
+        osc2.stop(time + 0.5);
     }
 
-    function stop(){
+    function playSnare(time) {
+        const osc = audioContext.createOscillator();
+        osc.type = 'triangle';
+        const gainOsc3 = audioContext.createGain();
+        gainOsc3.gain.setValueAtTime(1, time);
+        gainOsc3.gain.exponentialRampToValueAtTime(0.001, time + 1);
 
+        osc.frequency.setValueAtTime(frequency, time);
+        osc.frequency.exponentialRampToValueAtTime(0.001, time + 1);
+
+        osc.connect(gainOsc3);
+        gainOsc3.connect(mixGain);
+        osc.start(time);
+        osc.stop(time + 1);
+    }
+
+    function stop() {
+        window.clearTimeout(timerID);
+    }
+
+    function start() {
+        scheduler();
     }
 
     function togglePad(column, row) {
@@ -92,16 +150,28 @@
 <input type="range" min="90" max="300" bind:value={frequency} step="5" class="range">
 
 
-
-<button class="btn btn-primary" on:click={playKick}>Start</button>
+<button class="btn btn-primary" on:click={start}>Start</button>
 <button class="btn btn-primary" on:click={stop}>Stop</button>
 
 <div class="grid gap-4 grid-flow-col mt-4">
-    {#each grid as columm, c}
-        <div class="grid gap-4 grid-flow-row" style="width: 50px">
-            {#each columm as row, r}
-                <div class="bg-error border-solid border-2 border-orange-600 h-14 rounded" style="height: 50px" class:bg-error={row} on:click={()=>{togglePad(c,r)}}></div>
+    {#each grid as row, c}
+        <div class="grid gap-4 grid-flow-column ">
+            {#each row as column, r}
+                <div class="flex flex-row items-center justify-end">
+                    {#if c === 0}
+                        <div class="mr-16">{gridLabel[r]}</div>
+                    {/if}
+                    <div class="bg-error border-solid border-2 border-orange-600 h-14 rounded cell"
+                         class:bg-error={column} on:click={()=>{togglePad(c,r)}}></div>
+                </div>
             {/each}
         </div>
     {/each}
 </div>
+
+<style>
+    .cell {
+        height: 40px;
+        width: 40px;
+    }
+</style>
