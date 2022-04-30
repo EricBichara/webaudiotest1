ffbb24807e18113bc8bd0a23764ce49a955170bf
EricBichara <eric.bichara@gmail.com>
2022-04-30 21:48:32 +0200

    fixes to recorder settings

--- src/routes/recorder.svelte
+++ src/routes/recorder.svelte
@@ -33,38 +33,19 @@
     let selectedDevice = null;
     $: isBtnDisabled = selectedDevice === null;
 
-
     onMount(() => {
         if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
             alert('Your browser does not support recording!');
             return;
         }
 
-        navigator.mediaDevices.getUserMedia({audio: true}).then(() => {
+        navigator.mediaDevices.getUserMedia({audio: true, video: false}).then(() => {
             navigator.mediaDevices.enumerateDevices().then(function (deviceList) {
                 devices = deviceList.filter((device) => device.kind === 'audioinput');
             })
         });
     })
 
-    function mediaRecorderDataAvailable(e) {
-        chunks.push(e.data);
-    }
-
-    declare global {
-        interface Window {
-            URL: unknown;
-        }
-    }
-
-    function mediaRecorderStop() {
-        audioBlob = new Blob(chunks, {type: 'audio/mp4'});
-        let audioURL = window.URL.createObjectURL(audioBlob);
-        audioURLList = [...audioURLList, audioURL];
-        // reset to default
-        mediaRecorder = null;
-        chunks = [];
-    }
 
     function startRecord() {
         if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
@@ -74,10 +55,20 @@
 
         if (!mediaRecorder) {
             navigator.mediaDevices.getUserMedia({
-                audio: {deviceId: selectedDevice},
+                audio: {
+                    deviceId: selectedDevice,
+                    echoCancellation: false,
+                    autoGainControl: false,
+                    channelCount: 2,
+                    noiseSuppression: false,
+                    sampleRate: 48000
+                },
             })
                 .then((stream) => {
-                    mediaRecorder = new MediaRecorder(stream);
+                    mediaRecorder = new MediaRecorder(stream, {
+                        audioBitsPerSecond: 48000,
+                        mimeType: 'audio/webm;codecs=opus'
+                    });
                     mediaRecorder.start();
                     mediaRecorder.ondataavailable = mediaRecorderDataAvailable;
                     mediaRecorder.onstop = mediaRecorderStop;
@@ -90,5 +81,18 @@
             mediaRecorder.stop();
             text = "Record";
         }
+    }
+
+    function mediaRecorderDataAvailable(e) {
+        chunks.push(e.data);
+    }
+
+    function mediaRecorderStop() {
+        audioBlob = new Blob(chunks, {type: 'audio/webm'});
+        let audioURL = URL.createObjectURL(audioBlob);
+        audioURLList = [...audioURLList, audioURL];
+        // reset to default
+        mediaRecorder = null;
+        chunks = [];
     }
 </script>
