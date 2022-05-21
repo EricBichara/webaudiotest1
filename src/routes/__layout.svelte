8a4580f63fee492b7b737713964661a483ba20a5
EricBichara <eric.bichara@gmail.com>
2022-05-21 23:43:28 +0200

    added scss

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,44 +1,45 @@
 <script>
-    import '../app.scss';
-    import Navbar from '../components/Navbar.svelte';
+	import '../app.scss';
+	import '../app.scss';
+	import Navbar from '../components/Navbar.svelte';
 
-    let checked = false;
+	let checked = false;
 
-    function linkClicked() {
-        checked = !checked;
-    }
+	function linkClicked() {
+		checked = !checked;
+	}
 </script>
 
 <div class="w-full navbar bg-base-300">
-    <div class="flex-none lg:hidden">
-        <label for="my-drawer" class="btn btn-square btn-ghost">
-            <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    class="inline-block w-6 h-6 stroke-current"
-            >
-                <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M4 6h16M4 12h16M4 18h16"
-                />
-            </svg>
-        </label>
-    </div>
-    <a href="/" class="flex-1 px-2 mx-2">BAPP - The band&apos;s gonna make it</a>
+	<div class="flex-none lg:hidden">
+		<label for="my-drawer" class="btn btn-square btn-ghost">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				class="inline-block w-6 h-6 stroke-current"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M4 6h16M4 12h16M4 18h16"
+				/>
+			</svg>
+		</label>
+	</div>
+	<a href="/" class="flex-1 px-2 mx-2">BAPP - The band&apos;s gonna make it</a>
 </div>
 
 <div class="drawer drawer-mobile">
-    <input id="my-drawer" type="checkbox" class="drawer-toggle" bind:checked/>
-    <div class="drawer-content">
-        <div>
-            <slot/>
-        </div>
-    </div>
-    <div class="drawer-side">
-        <label for="my-drawer" class="drawer-overlay"></label>
-        <Navbar on:message={linkClicked}></Navbar>
-    </div>
+	<input id="my-drawer" type="checkbox" class="drawer-toggle" bind:checked />
+	<div class="drawer-content">
+		<div>
+			<slot />
+		</div>
+	</div>
+	<div class="drawer-side">
+		<label for="my-drawer" class="drawer-overlay" ></label>
+		<Navbar on:message={linkClicked} />
+	</div>
 </div>
